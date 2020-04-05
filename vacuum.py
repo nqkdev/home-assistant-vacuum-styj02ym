839f68ede5d3fa5921e4623396f2a38d27c4cc60
kovbalazs89 <58386184+kovbalazs89@users.noreply.github.com>
2020-04-05 18:41:34 +0200

    vacuum.py
    I have checked these parameters for my styj02ym vacuum. All of them showing the correct value in home assistant. Can you please add them to your code?

--- vacuum.py
+++ vacuum.py
@@ -120,7 +120,9 @@ STATE_CODE_TO_STATE = {
 }
 
 ALL_PROPS = ["run_state", "mode", "err_state", "battary_life", "box_type", "mop_type", "s_time",
-             "s_area", "suction_grade", "water_grade", "remember_map", "has_map", "is_mop", "has_newmap"]
+             "s_area", "suction_grade", "water_grade", "remember_map", "has_map", "is_mop", "has_newmap"
+             "side_brush_life", "side_brush_hours", "main_brush_life", "main_brush_hours", "hypa_life", "hypa_hours", "mop_life", "mop_hours", "water_percent",
+             "hw_info", "sw_info", "start_time", "order_time", "v_state", "zone_data", "repeat_state", "light_state", "is_charge", "is_work", "cur_mapid", "mop_route", "map_num"]
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
