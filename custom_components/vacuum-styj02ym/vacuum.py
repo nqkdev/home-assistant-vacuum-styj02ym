cd57b5dcac42da8d8f5ad52b245c46db92f50443
KrzysztofHajdamowicz <uosiu@uosiu.info>
2020-10-13 14:27:16 +0200

    Change deprecated StateVacuumDevice to StateVacuumEntity
    Home Assistant deprecated StateVacuumDevice, looks like simple `sed` is enough

--- custom_components/vacuum-styj02ym/vacuum.py
+++ custom_components/vacuum-styj02ym/vacuum.py
@@ -25,7 +25,7 @@ from homeassistant.components.vacuum import (
     SUPPORT_START,
     SUPPORT_STATE,
     SUPPORT_STOP,
-    StateVacuumDevice,
+    StateVacuumEntity,
 )
 from homeassistant.const import (
     ATTR_ENTITY_ID,
@@ -221,7 +221,7 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
         )
 
 
-class MiroboVacuum2(StateVacuumDevice):
+class MiroboVacuum2(StateVacuumEntity):
     """Representation of a Xiaomi Vacuum cleaner robot."""
 
     def __init__(self, name, vacuum):
