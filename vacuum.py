fcd7890cb761dfbb5ac1099e35a6a0011fa1f194
Nico Salvador <salvador.nico@gmail.com>
2022-03-09 01:02:25 +0800

    Replace deprecated classes

--- vacuum.py
+++ vacuum.py
@@ -3,7 +3,7 @@ import asyncio
 from functools import partial
 import logging
 
-from miio import DeviceException, Vacuum  # pylint: disable=import-error
+from miio import DeviceException, RoborockVacuum  # pylint: disable=import-error
 import voluptuous as vol
 
 from homeassistant.components.vacuum import (
@@ -134,7 +134,7 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 
   # Create handler
   _LOGGER.info("Initializing with host %s (token %s...)", host, token[:5])
-  vacuum = Vacuum(host, token)
+  vacuum = RoborockVacuum(host, token)
 
   mirobo = MiroboVacuum2(name, vacuum)
   hass.data[DATA_KEY][host] = mirobo
